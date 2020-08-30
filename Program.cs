471c711c310fc7acd7256bd048bc3cd6a55cd376
jananiAM <jananiam1997@gmail.com>
2020-08-30 19:27:34 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SquareApp;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(SquareNumber.Square(25));
+        }
+    }
+}
